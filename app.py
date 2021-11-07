d0d78c129b265a3d06c0d1101becb9efedc43cf9
jpbacher <bacherjp@gmail.com>
2021-11-07 08:57:58 -0500

    added mine_block function

--- app.py
+++ app.py
@@ -3,8 +3,23 @@ from flask import Flask, jsonify
 from blockchain import Blockchain
 
 
+# create blockchain
+blockchain = Blockchain()
+
 # create web app
 app = Flask(__name__)
 
-# create blockchain
-blockchain = Blockchain()
+@app.routee('/mine_block', methods=['GET'])
+def mine_block():
+    prev_block = blockchain.get_prev_block()
+    prev_proof = prev_block['proof']
+    proof = blockchain.pow(prev_proof)
+    prev_hash = blockchain.hash(prev_block)
+    block = blockchain.create_block(proof, prev_hash)
+    response = {'message': 'Just mined a block',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'prev_hash': block['prev_hash']}
+    return jsonify(response), 200
+ 
